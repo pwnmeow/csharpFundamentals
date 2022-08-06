9df42d0463d902f594161550584c1210010b88e2
sheeraz <hacker.ascool@gmail.com>
2022-08-07 01:23:18 +0530

    arrays and lists

---
+++ arraysdetailed/arraysdetailed/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace arraylists { 
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 0, 4, 3, 21, 9, 5, 93 };
+
+            // Length
+            Console.WriteLine("Length: "+ numbers.Length); 
+            
+            // IndexOf
+            var index = Array.IndexOf(numbers, 21);  
+            Console.WriteLine(index);
+
+            // Clear 
+            Console.WriteLine("Cleard the array");
+            Array.Clear(numbers, 0, 2);
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            // Copy 
+            int[] anotherarry = new int[3];
+            Console.WriteLine("Copied  the array");
+            Array.Copy(numbers, anotherarry, 2);
+
+            foreach (var item in anotherarry)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Sort 
+            Console.WriteLine("Sorted  the array");
+            Array.Sort(numbers);
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Reverse 
+            Console.WriteLine("Reversed  the array");
+            Array.Reverse(numbers);
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+
+}
---
+++ genericlists/genericlists/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace genericlists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // we can initialize the list ahead of time
+            // we can add 
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] {5,6,7});
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            // Index from start of arry
+
+            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+
+            // Index from end of arry
+           Console.WriteLine("Index of 1: " + numbers.LastIndexOf(1));
+
+            // Counts objects
+            Console.WriteLine("Count: " + numbers.Count());
+
+            // Remove from list first match
+            numbers.Remove(1);
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            // Remove all 1's from the list
+
+            for(var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+
+            Console.WriteLine("Now you have all the chars removed");
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+
+        }
+    }
+}
